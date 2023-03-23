2ca25697b6b36981472ec93e23330e2fce23608a
qamarali205-github <qamarali205@gmail.com.com>
2023-03-23 12:48:15 +0530

    reddit clone

--- src/components/welcome/Welcome.js
+++ src/components/welcome/Welcome.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import { UserAuth } from "../context/AuthContext";
+// import { UserAuth } from "../context/AuthContext";
 import './welcome.css'
 
 const Welcome = () => {
-    const {user}=UserAuth();
+    // const {user}=UserAuth();
   return (
     <div className='welcome'>
-      <p>Hi <span>{user.displayName}</span> Welcome to Reddit Clone Site.</p>
+      {/* <p>Hi <span>{user.displayName}</span> Welcome to Reddit Clone Site.</p> */}
     </div>
   )
 }
